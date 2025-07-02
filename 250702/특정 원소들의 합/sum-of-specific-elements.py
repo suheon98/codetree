620b67bb993cf33a70cfa2dcc9ed2b4b9132c1a4
suheon <dreasm12345@naver.com>
2025-07-02 16:18:31 +0900

    Title: 특정 원소들의 합, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250702/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,12 @@
+matrix = []
+
+for _ in range(4):
+    row = list(map(int, input().split()))  # 한 줄에 4개의 정수 입력
+    matrix.append(row)
+
+total = 0
+for i in range (4):
+    for j in range (i):
+        total += matrix[i][j]
+
+print(total)
