6561b0db5124998f55644fc97d79f5ee3ec8a115
suheon <dreasm12345@naver.com>
2025-07-03 08:59:21 +0900

    Title: 두 배열의 곱, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250703/두 배열의 곱/multiple-of-two-arrays.py
+++ 250703/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,14 +1,18 @@
+# N, M 입력
 N, M = map(int, input().split())
 
+# 첫 번째 격자 입력
 a = [list(map(int, input().split())) for _ in range(N)]
+
+# 두 번째 격자 입력
 b = [list(map(int, input().split())) for _ in range(N)]
 
-result = []
+# 결과 배열 생성
 for i in range(N):
     row = []
     for j in range(M):
-        row.append(0 if a[i][j] == b[i][j] else 1)
-    result.append(row)
-
-for row in result:
+        if a[i][j] == b[i][j]:
+            row.append(0)
+        else:
+            row.append(1)
     print(' '.join(map(str, row)))
