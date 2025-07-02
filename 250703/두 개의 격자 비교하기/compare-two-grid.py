b086038ad7f82ba0945043ff1d0e5ffa049e1bee
suheon <dreasm12345@naver.com>
2025-07-03 08:59:35 +0900

    Title: 두 개의 격자 비교하기, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250703/두 개의 격자 비교하기/compare-two-grid.py
+++ 250703/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,12 +1,12 @@
+# 첫 번째 3x3 배열 입력
 a = [list(map(int, input().split())) for _ in range(3)]
+
+# 두 번째 3x3 배열 입력
 b = [list(map(int, input().split())) for _ in range(3)]
 
-c = []
+# 결과 배열 생성
 for i in range(3):
     row = []
     for j in range(3):
         row.append(a[i][j] * b[i][j])
-    c.append(row)
-
-for row in c:
     print(' '.join(map(str, row)))
