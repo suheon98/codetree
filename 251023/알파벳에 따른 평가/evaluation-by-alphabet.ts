6123c1c857dd1128d7d6eb64dbaa9440789fac10
suheon <dreasm12345@naver.com>
2025-10-23 14:02:29 +0900

    Title: 알파벳에 따른 평가, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251023/알파벳에 따른 평가/evaluation-by-alphabet.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf-8").trim();
+
+if (input === "S") {
+  console.log("Superior");
+} else if (input === "A") {
+  console.log("Excellent");
+} else if (input === "B") {
+  console.log("Good");
+} else if (input === "C") {
+  console.log("Usually");
+} else if (input === "D") {
+  console.log("Effort");
+} else {
+  console.log("Failure");
+}
